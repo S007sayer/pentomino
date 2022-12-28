d85748a8f37b75ab368d15f43d6cef68acfc8343
Georgiy Belyanin <belyaningeorge@ya.ru>
2022-12-28 14:44:31 +0300

    Added a header for the terminal colors.

---
+++ color.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#define FG_COLOR_NORMAL "\x1B[0m"
+#define FG_COLOR_RED "\x1B[31m"
+#define FG_COLOR_GREEN "\x1B[32m"
+#define FG_COLOR_YELLOW "\x1B[33m"
+#define FG_COLOR_BLUE "\x1B[34m"
+#define FG_COLOR_MAGENTA "\x1B[35m"
+#define FG_COLOR_CYAN "\x1B[36m"
+#define FG_COLOR_WHITE "\x1B[37m"
+#define FG_COLOR_BOLD "\033[1m"
+
+#define BG_COLOR_NORMAL "\x1B[0m"
+#define BG_COLOR_RED "\x1B[41m"
+#define BG_COLOR_GREEN "\x1B[42m"
+#define BG_COLOR_YELLOW "\x1B[43m"
+#define BG_COLOR_BLUE "\x1B[44m"
+#define BG_COLOR_MAGENTA "\x1B[45m"
+#define BG_COLOR_CYAN "\x1B[46m"
+#define BG_COLOR_WHITE "\x1B[47m"
+
+typedef char* color_t;
